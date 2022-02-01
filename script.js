52857a0b9c2dea277a31031e246f74cb2f6c9dea
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-02-01 16:11:22 +0400

    lesson01

---
+++ script.js
@@ -0,0 +1,10 @@
+ const title = "title";
+ const screens = 5;
+ const screenPrice = 5;
+ const rollback = true;
+ const fullPrice = 5;
+ const adaptive = false;
+
+ alert('Погнали!');
+
+ console.log('Мне нужен рассол');
