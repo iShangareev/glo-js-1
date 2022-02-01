ecc3a6803cf9ebdc034ca38725d84527c272e908
Ильнар Шангареев <i.shangareyev@modulbank.ru>
2022-02-01 20:27:30 +0400

    Второе задание

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
- const title = "title";
- const screens = 5;
+ const title = "Goliaf";
+ const screens = "Простые, Сложные, Интерактивные";
  const screenPrice = 5;
- const rollback = true;
- const fullPrice = 5;
+ const rollback = 50;
+ const fullPrice = 500;
  const adaptive = false;
 
  alert('Погнали!');
 
  console.log('Мне нужен рассол');
+
+ console.log(typeof title, typeof fullPrice, typeof adaptive);
+ console.log(screens.length);
+ console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
+ console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+ console.log(screens.toLowerCase().split(","));
+ console.log(fullPrice * (rollback/100));
